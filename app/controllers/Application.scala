6067c42bea6b1f74831a264c10f4ec06daa802c3
remy <remy.auricoste@gmail.com>
2016-07-04 11:48:42 +0200

    prepare mock application for figarocms-api

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,9 +3,9 @@ package controllers
 import java.io.{File, FileOutputStream}
 
 import org.apache.commons.io.IOUtils
+import play.api.Play
 import play.api.Play.current
 import play.api.mvc.{Action, Controller}
-import play.api.{Logger, Play}
 
 object Application extends Controller {
 
@@ -13,10 +13,13 @@ object Application extends Controller {
   val os = new FileOutputStream(outputFile, true)
 
   def call(path: String) = Action(parse.tolerantText) { implicit request =>
-    val message = s"${request.method} /$path ${request.rawQueryString} ${request.body}"
+    val message = s"${request.method} /$path ${request.rawQueryString} ${request.body.hashCode} ${request.body.replaceAll("\n", " ")}"
     IOUtils.write(message+"\n", os)
-    Logger.info(message)
-
-    Ok("")
+//    Logger.info(message)
+    path match {
+      case string: String if string.startsWith("v2/partners/") => if (path.hashCode % 2 == 0) Ok else InternalServerError("random error")
+      case string: String if string.contains("_search") => Ok("{}")
+      case _ => Ok("unknown path")
+    }
   }
 }
